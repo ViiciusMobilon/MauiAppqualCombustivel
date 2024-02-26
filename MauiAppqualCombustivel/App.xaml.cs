88035aef4ae22f3d17dbc3a5fee5fdac1b799d38
Usuario <Usuario@CPD03-M07>
2024-02-26 14:05:42 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppqualCombustivel/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppqualCombustivel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppqualCombustivel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppqualCombustivel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
